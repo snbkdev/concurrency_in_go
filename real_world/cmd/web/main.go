5ab3e2e8d0937de14f4fa7ca2047a9c5969a7340
Asanbek09 <samudinua@gmail.com>
2026-09-07 14:49:39 +0600

    Проект старый

---
+++ real_world/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const WebPort = "8080"
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	// set up the application config
+
+	// set up mail
+
+	// listen for web connections
+
+}
